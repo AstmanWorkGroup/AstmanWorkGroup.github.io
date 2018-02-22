b2a3e1bf146eff925971e57f3bb7acaad13e8442
AstmanWorkGroup <astman555@gmail.com>
2018-02-22 11:25:37 +0300

    Revert "init commit"
    This reverts commit 8743bebc22ff8e5bdc7e07bf19c3bbfd22ad7cbf.

--- pages/account/_referals2.php
+++
@@ -1,9 +0,0 @@
-	<div class="acc-title">Устроиться на работу</div>
-</div>
-<center>Цена 300руб обрашение: skype: g.r.pro</center>
-<div class="silver-bk"><div class="clr"></div>	
-
-</div>
-<br/><br/>
-
-</div>
